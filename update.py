69964c39833d467b2d8d968931d2a5eb77f44878
jiahuanting <jht1995@126.com>
2020-03-28 23:09:07 +0800

    20200328

--- update.py
+++ update.py
@@ -340,7 +340,7 @@ def china_map(indextype):
 if __name__=='__main__':
     # today=strftime(r"%Y%m%d",localtime(time()))
 
-    today=20200325
+    today=20200327
     bottomCard_quegong(today)
     bottomCard_fugong(today)
     calFugong(start=20200210, end=today)
